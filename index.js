3ad79e9dd6311acec05923b3f5e24e13c8f054d3
pj <pjhg14@gmail.com>
2021-03-21 20:47:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(names, matcher) {
+    return names.filter(driver => driver.toLowerCase() === matcher.toLowerCase())
+}
+
+function fuzzyMatch(names, firstInitial) {
+    return names.filter(driver => driver[0] === firstInitial[0])
+}
+
+function matchName(names, name) {
+    return names.filter(driver => driver.name === name)
+}
